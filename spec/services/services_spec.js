90a07bdf8e1ad5b2eb0b3ed0a85dad983c900f8c
agvozdikov <andrey.gvozdikov@fastdev.se>
2015-08-13 11:22:47 +0200

    first commit

---
+++ spec/services/services_spec.js
@@ -0,0 +1,20 @@
+var jsonServer = require('json-server');
+var frisby = require('frisby');
+
+var server = jsonServer.create(); // Returns an Express server
+server.use(jsonServer.defaults); // logger, static and cors middlewares
+server.listen(3003);
+
+describe("Services specs", function() {
+
+  var router = jsonServer.router('services.json'); // Returns an Express router
+  server.use('/api', router); // Mount router on '/'
+
+  it("retrive a list of services", function() {
+
+    frisby.create('Get single-deck')
+        .get("http://localhost:3003/api/services/")
+        .expectStatus(200)
+        .toss();
+  });
+});
